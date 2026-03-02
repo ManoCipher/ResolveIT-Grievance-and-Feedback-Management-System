837b068ac328d35dce2dd3a9ae07969f02be5c7d
YOUR NAME <mano720051@gmail.com>
2026-03-02 15:28:08 +0530

    Configured Tailwind CSS successfully

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-red-500">
+      <h1 className="text-5xl font-extrabold text-white">
+        Tailwind Working Perfectly 🔥
+      </h1>
+    </div>
+  );
+}
+
+export default App;
