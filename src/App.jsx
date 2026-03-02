785c9861ac537d9be1841a40dc6596793a6fa1b9
YOUR NAME <mano720051@gmail.com>
2026-03-02 15:35:38 +0530

    Installed react-router-dom and fixed routing error

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function AdminDashboard() {
+  return <div className="p-6 text-2xl">Admin Dashboard</div>;
+}
+
+function StudentDashboard() {
+  return <div className="p-6 text-2xl">Student Dashboard</div>;
+}
+
+function ResolverDashboard() {
+  return <div className="p-6 text-2xl">Resolver Dashboard</div>;
+}
+
 function App() {
   return (
-    <div className="h-screen flex items-center justify-center bg-red-500">
-      <h1 className="text-5xl font-extrabold text-white">
-        Tailwind Working Perfectly 🔥
-      </h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/student" element={<StudentDashboard />} />
+        <Route path="/resolver" element={<ResolverDashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
